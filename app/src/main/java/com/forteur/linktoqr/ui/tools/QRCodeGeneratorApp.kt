224d6eb17c8194b0a3237e0113984f5fb420b3fe
Daniele Della Cioppa <daniele.dellacioppa@gmail.com>
2024-06-12 15:47:45 +0100

    ui

--- app/src/main/java/com/forteur/linktoqr/ui/tools/QRCodeGeneratorApp.kt
+++ app/src/main/java/com/forteur/linktoqr/ui/tools/QRCodeGeneratorApp.kt
@@ -29,11 +29,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.forteur.linktoqr.R
 import com.forteur.linktoqr.ui.theme.LINKtoQRTheme
 import com.google.zxing.BarcodeFormat
 import com.google.zxing.WriterException
@@ -46,66 +50,95 @@ fun QRCodeGeneratorApp() {
     var qrCodeBitmap by remember { mutableStateOf<Bitmap?>(null) }
     val context = LocalContext.current
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-            .background(Color.LightGray),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        OutlinedTextField(
-            value = link,
-            onValueChange = { link = it },
-            label = { Text(text = "URL to QRify") },
-            placeholder = { Text(text = "Enter URL") },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
-            singleLine = true,
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = Color.Blue,
-                unfocusedBorderColor = Color.Gray,
-                cursorColor = Color.Black,
-            ),
-            shape = MaterialTheme.shapes.medium
+        // Background Image
+        Image(
+            painter = painterResource(id = R.drawable.wallp1),
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize()
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {
-            if (link.isEmpty()) {
-                Toast.makeText(context, "Please enter a link", Toast.LENGTH_SHORT).show()
-            } else {
-                try {
-                    val barcodeEncoder = BarcodeEncoder()
-                    val bitmap = barcodeEncoder.encodeBitmap(link, BarcodeFormat.QR_CODE, 400, 400)
-                    qrCodeBitmap = bitmap
-                } catch (e: WriterException) {
-                    e.printStackTrace()
-                }
-            }
-        }) {
-            Text(text = "Generate QR Code")
-        }
-        Spacer(modifier = Modifier.height(16.dp))
-        qrCodeBitmap?.let { bitmap ->
-            Card(
-                shape = MaterialTheme.shapes.medium,
-                modifier = Modifier
-                    .size(220.dp),
-                elevation = CardDefaults.cardElevation(8.dp),
-                colors = CardDefaults.cardColors(
-                    containerColor = Color.White
-                )
-            ) {
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Image(
-                        bitmap = bitmap.asImageBitmap(),
-                        contentDescription = "QR Code",
-                        modifier = Modifier.fillMaxSize()
+
+        // Gradient overlay
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Black.copy(alpha = 0.8f),
+                            Color.Transparent,
+                            Color.Transparent,
+                            Color.Black.copy(alpha = 0.8f)
+                        ),
+                        startY = 0f,
+                        endY = Float.POSITIVE_INFINITY
                     )
+                )
+        )
+
+        // Foreground Content
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            OutlinedTextField(
+                value = link,
+                onValueChange = { link = it },
+                label = { Text(text = "URL to QRify") },
+                placeholder = { Text(text = "Enter URL") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                singleLine = true,
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Blue,
+                    unfocusedBorderColor = Color.Gray,
+                    cursorColor = Color.Black,
+                ),
+                shape = MaterialTheme.shapes.medium
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(onClick = {
+                if (link.isEmpty()) {
+                    Toast.makeText(context, "Please enter a link", Toast.LENGTH_SHORT).show()
+                } else {
+                    try {
+                        val barcodeEncoder = BarcodeEncoder()
+                        val bitmap = barcodeEncoder.encodeBitmap(link, BarcodeFormat.QR_CODE, 400, 400)
+                        qrCodeBitmap = bitmap
+                    } catch (e: WriterException) {
+                        e.printStackTrace()
+                    }
+                }
+            }) {
+                Text(text = "Generate QR Code")
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            qrCodeBitmap?.let { bitmap ->
+                Card(
+                    shape = MaterialTheme.shapes.medium,
+                    modifier = Modifier.size(220.dp),
+                    elevation = CardDefaults.cardElevation(8.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    )
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        Image(
+                            bitmap = bitmap.asImageBitmap(),
+                            contentDescription = "QR Code",
+                            modifier = Modifier.fillMaxSize()
+                        )
+                    }
                 }
             }
         }
